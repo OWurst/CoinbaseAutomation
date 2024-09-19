ff0bcaff5c69ca8db9d36d0d83bacb66386a5371
Owen Wurst <owenwurst82@gmail.com>
2024-09-18 22:13:27 -0400

    add queries and inserts

--- coinbaseapi/database_interface.py
+++ coinbaseapi/database_interface.py
@@ -38,3 +38,76 @@ class DatabaseInterface:
             )
         ''')
         c.close()
+
+    def store_transaction(self, transaction):
+        conn = sqlite3.connect(self.db_filename)
+        c = conn.cursor()
+        c.execute('''
+            INSERT INTO transactions (type, amount, asset, value, timestamp)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (transaction.type, transaction.amount, transaction.asset, transaction.value, transaction.timestamp))
+        c.close()
+        conn.commit()
+        conn.close()
+
+    def store_holding(self, holding):
+        conn = sqlite3.connect(self.db_filename)
+        c = conn.cursor()
+        c.execute('''
+            INSERT INTO holdings (asset, amount, value)
+            VALUES (?, ?, ?)
+            ON CONFLICT(asset) DO UPDATE SET 
+                amount = excluded.amount,
+                value = excluded.value
+        ''', (holding.asset, holding.amount, holding.value))
+        c.close()
+        conn.commit()
+        conn.close()
+    
+    def get_transactions(self):
+        conn = sqlite3.connect(self.db_filename)
+        c = conn.cursor()
+        c.execute('''
+            SELECT * FROM transactions
+        ''')
+        transactions = c.fetchall()
+        c.close()
+        conn.close()
+
+        return transactions
+    
+    def get_holdings(self):
+        conn = sqlite3.connect(self.db_filename)
+        c = conn.cursor()
+        c.execute('''
+            SELECT * FROM holdings
+        ''')
+        holdings = c.fetchall()
+        c.close()
+        conn.close()
+
+        return holdings
+    
+    def get_transactions_by_asset(self, asset):
+        conn = sqlite3.connect(self.db_filename)
+        c = conn.cursor()
+        c.execute('''
+            SELECT * FROM transactions WHERE asset = ?
+        ''', (asset,))
+        transactions = c.fetchall()
+        c.close()
+        conn.close()
+
+        return transactions
+    
+    def get_holdings_by_asset(self, asset):
+        conn = sqlite3.connect(self.db_filename)
+        c = conn.cursor()
+        c.execute('''
+            SELECT * FROM holdings WHERE asset = ?
+        ''', (asset,))
+        holdings = c.fetchall()
+        c.close()
+        conn.close()
+
+        return holdings
